8d2e5dcb356160bbf3bbdc1175a92dd2ac31bafd
Pierre <pierre@hubert.fr>
2018-02-23 11:41:32 +0100

    Fixed issue in mail verification

--- assets/js/common/utils.js
+++ assets/js/common/utils.js
@@ -177,7 +177,7 @@ function clearObject(object){
  * @return {Boolean} True for a valid email address / false else
  */
 function checkMail(emailAddress){
-	return (emailAddress.match(/^[a-zA-Z0-9_.]+@[a-zA-Z0-9-]{1,}[.][a-zA-Z]{2,5}$/) === null ? false : true);
+	return (emailAddress.match(/^[a-zA-Z0-9_.]+@[a-zA-Z0-9-.]{1,}[.][a-zA-Z]{2,5}$/) === null ? false : true);
 }
 
 /**
